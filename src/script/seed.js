11a9b31f77c7d69102664541b99793f5a2d9053a
AtomicZero <atomiczero@gmail.com>
2020-09-12 14:10:22 +0100

    updated sleepMood Schema and seed info

---
+++ src/script/seed.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+import models from '../models/sleepMood';
+import { DB_URI, MONGOOSE_OPTIONS } from '../config';
+import sleepMoods from '../data/sleep-n-mood-seed';
+
+mongoose.connect(DB_URI, MONGOOSE_OPTIONS);
+
+models.sleepMood.deleteMany({})
+  .then(() => models.sleepMood.collection.insertMany(sleepMoods))
+  .then((data) => {
+    console.log(`${data.result.n} records inserted!`);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
