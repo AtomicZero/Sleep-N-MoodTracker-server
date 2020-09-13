c68bce2b90b06b1fd760c5d27295f72b5c3d0eca
AtomicZero <atomiczero@gmail.com>
2020-09-13 11:11:33 +0100

    added 3 more routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,32 @@
 import express from 'express';
 
+import models from '../models';
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Hello');
 });
+
+const getAllsleepMoods = async (_, res) => {
+  const sleepMoods = await models.sleepMood.find({});
+  res.json({ sleepMoods });
+};
+
+const getWeekByID = async (req, res) => {
+  const { weekID } = req.params;
+  const sleepMood = await models.sleepMood.findById(weekID);
+  res.json({ sleepMood });
+};
+
+const getDate = async (req, res) => {
+  const { Date } = req.params;
+  const sleepMood = await models.sleepMood.findOne({ Date });
+  res.json({ sleepMood });
+};
+
+router.get('/sleepMoods', getAllsleepMoods);
+router.get('/date', getDate);
+router.get('/week/:id', getWeekByID);
 
 export default router;
