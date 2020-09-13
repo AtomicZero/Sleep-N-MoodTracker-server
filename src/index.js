5dd2ef74f8bbc8bce67f035189270919fabc8b1d
AtomicZero <atomiczero@gmail.com>
2020-09-13 13:26:29 +0100

    added auth file with updated routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import auth from './routes/auth';
 
 import routes from './routes';
 import { PORT, DB_URI, MONGOOSE_OPTIONS } from './config';
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/api', routes);
+app.use('/auth', auth);
 
 mongoose.connect(DB_URI, MONGOOSE_OPTIONS);
 
